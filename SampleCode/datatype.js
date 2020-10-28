05a7a74d2741af6f0dd3dab07383bf78bb2557fb
Ray Kuo <ray20140811@gmail.com>
2020-10-28 21:33:45 +0800

    add file

---
+++ SampleCode/datatype.js
@@ -0,0 +1,27 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object 
+*/
+var a;
+console.log("a=" + a); //undefined
+console.log(typeof(a)); //undefined
+
+var b = null;
+console.log("b=" + b); //null
+console.log(typeof(b)); //object
+
+var isTrue = true;
+console.log("isTrue=" + isTrue);
+console.log(typeof(isTrue)); //boolean
+
+var myName = "Ray"
+console.log("myName=" + myName);
+console.log(typeof(myName)); //string
+
+myName = 8
+console.log("myName=" + myName);
+console.log(typeof(myName)); //number
+
+const pi = 3.14
+
+console.log("pi=" + pi);
+console.log(typeof(pi)); //number
